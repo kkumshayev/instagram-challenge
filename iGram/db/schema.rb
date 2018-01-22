c38105e9e535d3f5edfedc921c5023f067049ff2
kkumshayev <kumshkesh@gmail.com>
2018-01-22 00:00:23 +0000

    unable to figure out image uploading, attempted with paperclip

--- iGram/db/schema.rb
+++ iGram/db/schema.rb
@@ -10,14 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180121181318) do
-
-  create_table "comments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20180121235235) do
 
   create_table "images", force: :cascade do |t|
+    t.text "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
